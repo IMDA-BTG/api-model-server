f29944a1d668bc453faa678aef3f4e48d20a8cf5
Benedict Lee <benedict_lee@imda.gov.sg>
2024-02-07 22:10:07 +0800

    Update Server IP Address Broadcast

--- classification_run_https.sh
+++ classification_run_https.sh
@@ -1,1 +1,1 @@
-export FLASK_APP=classification_server.py && flask run --cert=cert.pem --key=key.pem -p 5001
+export FLASK_APP=classification_server.py && flask run --cert=cert.pem --key=key.pem -p 5001 --host=0.0.0.0
--- regression_run_https.sh
+++ regression_run_https.sh
@@ -1,1 +1,1 @@
-export FLASK_APP=regression_server.py && flask run --cert=cert.pem --key=key.pem
+export FLASK_APP=regression_server.py && flask run --cert=cert.pem --key=key.pem --host=0.0.0.0
